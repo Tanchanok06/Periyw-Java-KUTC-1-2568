2e9f1e564b6b63aacea072151659c316d8175388
tanchanok06 <tanchanok4499tho@gail.com>
2025-06-13 11:58:21 +0700

    update file hellowld and variables

---
+++ src/com/project/java_basic/TypeVariables.java
@@ -0,0 +1,21 @@
+package com.project.java_basic;
+public class TypeVariables {
+    public static void main(String[] args) {
+        String name = "Periyw";//ประกาศตัวแปรแบบข้อความ
+        System.out.println(name);
+
+        int number = 24;
+        System.out.println(number);
+
+
+        double num = 15.44;
+        char A = 'b';
+        boolean result = false;
+        // Ture แปลว่า เป็นจริง
+        // False แปลว่า เท็จ
+
+        System.out.println(A+num+"Periyw");
+
+
+    }
+}
